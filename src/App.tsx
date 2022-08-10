21524a755d3940b1866de3e23a908b964c93c677
roivasxxx <tiragoshi@gmail.com>
2022-08-10 16:06:39 +0200

    playing around with styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,10 @@ import "./styles/app.css";
 
 function App() {
   return (
-    <div>
-      <img src={Logo} alt="" />
-      <h1>Hello React TS</h1>
+    <div className="bg-stone-800 w-full h-full">
+      <div className="flex justify-center align-center w-1/2 m-auto">
+        <h1 className="title">Hello React TS</h1>
+      </div>
       <div className="text-green-400">test</div>
     </div>
   );
